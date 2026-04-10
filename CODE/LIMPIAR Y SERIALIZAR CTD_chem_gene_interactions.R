185cc8ae83f9ca1af0f3f592a1bf608e74b19e4f
mariacanovfandos <mariacf2003@gmail.com>
2026-04-10 18:40:14 +0200

    Update LIMPIAR Y SERIALIZAR CTD_chem_gene_interactions.R

--- CODE/LIMPIAR Y SERIALIZAR CTD_chem_gene_interactions.R
+++ CODE/LIMPIAR Y SERIALIZAR CTD_chem_gene_interactions.R
@@ -4,20 +4,23 @@
 # 1. Cargar librerías
 library(dplyr)
 library(readr)
+library(data.table)
+library(tidyverse)
 
 # 2. Leer archivo
-datos_quimico_gen <- read.csv("TFG/DB/CTD_chem_gene_interactions.csv", skip = 27)
+datos_quimico_gen <- fread("https://ctdbase.org/reports/CTD_chem_gene_ixns.csv.gz", skip = 27, stringsAsFactors = FALSE)
 
-# 3. Limpiar archivo
-enfermedad_ruta_limpio <- datos_enfermedad_ruta %>%
-  rename(DiseaseName = 1) %>%
-  # Seleccionar columnas de interés
-  select(DiseaseName, DiseaseID, PathwayName, PathwayID) %>%
+# 3. Limpieza archivo
+datos_quimico_gen <- datos_quimico_gen %>%
+  set_names(c("ChemicalName", "ChemicalID", "CasRN", "GeneSymbol", "GeneID", 
+              "GeneForms", "Organism", "OrganismID", "Interaction", 
+              "InteractionActions", "PubMedIDs")) %>%
+  select(ChemicalID, GeneSymbol, OrganismID, InteractionActions, PubMedIDs) %>%
   mutate(across(everything(), as.character)) %>%
   mutate(across(everything(), ~na_if(., ""))) %>%
   slice(-1) %>%
-  # Filtro de seguridad
-  filter(!is.na(DiseaseID) & !is.na(PathwayID))
+  filter(OrganismID == "9606") %>%
+  filter(!is.na(ChemicalID) & !is.na(GeneSymbol))
 
 
 # ----------- SERIALIZACIÓN ------------
@@ -26,8 +29,7 @@ enfermedad_ruta_limpio <- datos_enfermedad_ruta %>%
 ruta_guardado_cg <- "TFG/RESULTADOS/chem_gene_association_ctd.ttl"
 cat("", file = ruta_guardado_cg)
 
-# 2. Prefijos
-rdf_ns <- "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+rdf <- "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 rdfs <- "http://www.w3.org/2000/01/rdf-schema#"
 sio <- "http://semanticscience.org/resource/"
 schema <- "https://schema.org/"
@@ -35,21 +37,24 @@ bao <- "http://www.bioassayontology.org/bao#"
 
 mesh_prefix <- "http://id.nlm.nih.gov/mesh/"
 gene_base <- "http://rdf.biogateway.eu/gene/9606/" 
-assoc_base <- "http://tfg.org/asociacion_cg/" 
+assoc_base <- "http://tfg.org/chemical_gene_association/" 
 pubmed_base <- "https://pubmed.ncbi.nlm.nih.gov/"
 
-# 3. Bucle
-for (i in 1:nrow(quimico_gen_limpio)) {
+# 2. Bucle
+for (i in 1:nrow(datos_quimico_gen)) {
   
   # Extraer datos
-  chem_id <- quimico_gen_limpio$ChemicalID[i]
-  gene_symbol <- quimico_gen_limpio$GeneSymbol[i]
-  interaccion_val <- quimico_gen_limpio$InteractionActions[i]
-  pubmed <- quimico_gen_limpio$PubMedIDs[i]
+  chem_id <- datos_quimico_gen$ChemicalID[i]
+  gene_symbol <- datos_quimico_gen$GeneSymbol[i]
+  interaccion_val <- datos_quimico_gen$InteractionActions[i]
+  pubmed <- datos_quimico_gen$PubMedIDs[i]
   
-  # Crear URIs
-  uri_asociacion <- paste0("<", assoc_base, i, ">")
-  uri_quimico <- paste0("<", mesh_prefix, sub("MESH:", "", chem_id), ">")
+  # Limpiamos el "MESH:" del identificador del químico por si acaso
+  chem_limpio <- sub("MESH:", "", chem_id)
+  
+  # CORRECCIÓN 2: Crear URI determinista (Químico--Gen) en lugar de usar 'i'
+  uri_asociacion <- paste0("<", assoc_base, chem_limpio, "--", gene_symbol, ">")
+  uri_quimico <- paste0("<", mesh_prefix, chem_limpio, ">")
   uri_gen <- paste0("<", gene_base, gene_symbol, ">")
   
   # Vector temporal para guardar las líneas de texto de esta iteración
@@ -57,21 +62,21 @@ for (i in 1:nrow(quimico_gen_limpio)) {
   
   # Tripletas Estructurales
   lineas <- c(lineas, paste(uri_asociacion, 
-                            paste0("<", rdf_ns, "type>"), 
+                            paste0("<", rdf, "type>"), 
                             paste0("<", sio, "SIO_001257> .")))
   
   lineas <- c(lineas, paste(uri_asociacion, 
                             paste0("<", rdfs, "subject>"), 
                             uri_quimico, "."))
   lineas <- c(lineas, paste(uri_quimico, 
-                            paste0("<", rdf_ns, "type>"), 
+                            paste0("<", rdf, "type>"), 
                             paste0("<", sio, "SIO_010004> .")))
   
   lineas <- c(lineas, paste(uri_asociacion, 
                             paste0("<", rdfs, "object>"), 
                             uri_gen, "."))
   lineas <- c(lineas, paste(uri_gen, 
-                            paste0("<", rdf_ns, "type>"), 
+                            paste0("<", rdf, "type>"), 
                             paste0("<", sio, "SIO_010035> .")))
   
   # Tripletas de InteractionActions (BAO)
@@ -95,7 +100,7 @@ for (i in 1:nrow(quimico_gen_limpio)) {
                                 paste0("<", sio, "SIO_000772>"), 
                                 uri_articulo, "."))
       lineas <- c(lineas, paste(uri_articulo, 
-                                paste0("<", rdf_ns, "type>"), 
+                                paste0("<", rdf, "type>"), 
                                 paste0("<", sio, "SIO_000154> .")))
       lineas <- c(lineas, paste(uri_articulo, 
                                 paste0("<", schema, "identifier>"), 
@@ -108,6 +113,6 @@ for (i in 1:nrow(quimico_gen_limpio)) {
   
   # Avance
   if (i %% 10000 == 0) {
-    message(paste("Procesadas", i, "filas de", nrow(quimico_gen_limpio)))
+    message(paste("Procesadas", i, "filas de", nrow(datos_quimico_gen)))
   }
 }
